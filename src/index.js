305900cbad38d00cfb4669bcf9f06f50390b2dbb
ScriptCamilo <rodrigo.camilo228@gmail.com>
2021-06-12 08:07:05 -0300

    feat: adding dependencies

--- src/index.js
+++ src/index.js
@@ -1,11 +1,24 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
+import { BrowserRouter as Router } from 'react-router-dom';
+import { Provider } from 'react-redux';
+import { ThemeProvider } from 'styled-components';
+import store from './store';
+
+import GlobalStyle from './styles/global';
+import light from './styles/themes/light'
 import App from './App';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Router>
+      <Provider store={ store }>
+        <ThemeProvider theme={ light }>
+          <GlobalStyle />
+          <App />
+        </ThemeProvider>
+      </Provider>
+    </Router>
   </React.StrictMode>,
   document.getElementById('root')
 );
